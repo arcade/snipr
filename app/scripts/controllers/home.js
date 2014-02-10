1c5e3a126c2103057fb4361b0ab4591ef5bb0ad1
Alex Bularca <abu@everymatrix.com>
2014-02-10 17:40:43 +0200

    reorganise markup

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('sniprApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('HomeCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
